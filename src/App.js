eb37507d1281b4b3ff2f04370ece6d06d236779c
Jay <116904048+Vegasaga@users.noreply.github.com>
2023-03-23 13:25:33 -0700

    added API and Axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
-        <main>
-          <Dictionary />
-        </main>
-        <footer className="text-center">Coded by Johanna Sanchez</footer>
+        <header className="App-header"></header>
+        <Dictionary defaultKeyword="sunset" />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Johanna Sanchez
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/shecodesio/dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://velvety-parfait-8153f4.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
 }
+
+export default App;
